4af7d50e3a552442dd64036f8dbf25a87050b4d1
Matias <lodigiani.matias97@gmail.com>
2022-09-21 14:37:22 -0300

    First commit

---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    primary: '#2292A4',
+    secondary: '#D96C06',
+    background: '#ffffff',
+    backgroundLight: '#F5EFED',
+    backgrouyndDark: '#',
+    white: '#ffffff',
+    black: '#000000',
+    text: '#212121',
+}
